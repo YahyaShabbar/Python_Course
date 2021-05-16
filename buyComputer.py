78255e2f2c82308abcca783ef0748f8feee2e746
Yahya <71684922+YahyaShabbar@users.noreply.github.com>
2021-05-16 13:48:31 -0700

    Update buyComputer.py

--- buyComputer.py
+++ buyComputer.py
@@ -4,6 +4,16 @@ computer_parts = []
 while current_choice != '0':
     if current_choice in "12345":
         print("Adding {}".format(current_choice))
+        if current_choice == '1':
+            computer_parts.append("Computer")
+        elif current_choice == '2':
+            computer_parts.append('Monitor')
+        elif current_choice == '3':
+            computer_parts.append('Keyboard')
+        elif current_choice == '4':
+            computer_parts.append('Mouse')
+        elif current_choice == '5':
+            computer_parts.append('Mouse Mat')
 
     else:
         print("Please add options from the list below:")
