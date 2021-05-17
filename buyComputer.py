1c8fb3d394fc31dadf98f7cb2183ebc2541db023
Yahya <71684922+YahyaShabbar@users.noreply.github.com>
2021-05-16 18:55:19 -0700

    Update buyComputer.py

--- buyComputer.py
+++ buyComputer.py
@@ -1,27 +1,36 @@
-current_choice = "-"
-computer_parts = []
+availableParts = ["Computer", "Monitor", "Keyboad", "Mouse", "Mouse Mat", "HDMI Cable", "DVD Drive"]
 
-while current_choice != '0':
-    if current_choice in "12345":
-        print("Adding {}".format(current_choice))
-        if current_choice == '1':
-            computer_parts.append("Computer")
-        elif current_choice == '2':
-            computer_parts.append('Monitor')
-        elif current_choice == '3':
-            computer_parts.append('Keyboard')
-        elif current_choice == '4':
-            computer_parts.append('Mouse')
-        elif current_choice == '5':
-            computer_parts.append('Mouse Mat')
+# validChoices = [str(i) for i in range(1, len(available_parts) + 1)]
+
+validChoices = []
+for i in range(1, len(availableParts) + 1):
+    validChoices.append(str(i))
+
+print(validChoices)
+
+currentChoice = "-"
+computerParts = []
+
+while currentChoice != '0':
+    if currentChoice in validChoices:
+        print("Adding {}".format(currentChoice))
+        index = int(currentChoice) - 1
+        chosenPart = availableParts[index]
+        if chosenPart in computerParts:
+            # it's already in there
+            print("Removing {}".format(currentChoice))
+            computerParts.remove(chosenPart)
+            
+        else:
+            print("Adding {}".format(currentChoice))
+            computerParts.append(chosenPart)
+        print("Your list now contains: {}".format(computerParts))
 
     else:
         print("Please add options from the list below:")
-        print("1: Computer")
-        print("2: Monitor")
-        print("3: Keyboard")
-        print("4: Mouse")
-        print("5: Mouse Mat")
-        print("0: To Finish")
+        for number, part in enumerate(availableParts):
+            print("{0}: {1}".format(number + 1, part))
 
-    current_choice = input("")
+    currentChoice = input("")
+
+print(computerParts)
